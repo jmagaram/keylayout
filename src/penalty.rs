5810d1762bd886781224c32add1fc2f5711e9e59
jmagaram <justin@magaram.com>
2024-04-06 16:27:44 -0700

    display trait

---
+++ src/penalty.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+#[derive(PartialEq, PartialOrd, Debug, Clone, Copy)]
+pub struct Penalty(f32);
+
+impl fmt::Display for Penalty {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Penalty(penalty) = self;
+        let prefix = '\u{1D561}';
+        write!(f, "{}{:.3}", prefix, penalty)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn display_property() {
+        let tests = [0.002, 0.0235, 0.0, 1.43, 0.0285, 6.0];
+        tests.into_iter().for_each(|p| {
+            let p = Penalty(p);
+            println!("The penalty is {}", p);
+        })
+    }
+}
