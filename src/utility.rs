3ffa48f366791b144cd8b26d98782f3cf2ef25ab
jmagaram <justin@magaram.com>
2024-04-06 14:24:29 -0700

    partitions as struct

--- src/utility.rs
+++ src/utility.rs
@@ -1,23 +1,41 @@
-fn partitions(sum: u32, parts: u32, min: u32, max: u32) -> Vec<Vec<u32>> {
-    match sum == 0 {
-        true => vec![vec![]],
-        false => (min..=max)
-            .into_iter()
-            .filter_map(|n| match n + (min * parts - 1) <= sum && n * parts >= sum {
-                true => Some(n),
-                false => None,
-            })
-            .into_iter()
-            .flat_map(|digit| {
-                let m = partitions(sum - digit, parts - 1, min, digit);
-                let zz = m.into_iter().map(move |dd| {
-                    let mut ddCopy = dd.clone();
-                    ddCopy.push(digit);
-                    ddCopy
-                });
-                zz
-            })
-            .collect::<Vec<Vec<u32>>>(),
+pub struct Partitions {
+    sum: u32,
+    parts: u32,
+    min: u32,
+    max: u32,
+}
+
+impl Partitions {
+    fn partitions(&self) -> Vec<Vec<u32>> {
+        match self.sum == 0 {
+            true => vec![vec![]],
+            false => (self.min..=self.max)
+                .into_iter()
+                .filter_map(|n| {
+                    match n + (self.min * self.parts - 1) <= self.sum && n * self.parts >= self.sum
+                    {
+                        true => Some(n),
+                        false => None,
+                    }
+                })
+                .into_iter()
+                .flat_map(|digit| {
+                    let m = Partitions {
+                        sum: self.sum - digit,
+                        parts: self.parts - 1,
+                        min: self.min,
+                        max: digit,
+                    }
+                    .partitions();
+                    let zz = m.into_iter().map(move |dd| {
+                        let mut ddCopy = dd.clone();
+                        ddCopy.push(digit);
+                        ddCopy
+                    });
+                    zz
+                })
+                .collect::<Vec<Vec<u32>>>(),
+        }
     }
 }
 
@@ -32,8 +50,14 @@ mod tests {
         let parts = 3;
         let min = 1;
         let max = sum;
-        partitions(sum, parts, min, max)
-            .into_iter()
-            .for_each(|p| println!("{:?}", p))
+        Partitions {
+            sum: sum,
+            parts: parts,
+            min: min,
+            max: max,
+        }
+        .partitions()
+        .into_iter()
+        .for_each(|p| println!("{:?}", p))
     }
 }
