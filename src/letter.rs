b580349691ea67e689a6dafe3051202ace4c20b2
jmagaram <justin@magaram.com>
2024-04-10 17:45:01 -0700

    with letter too

--- src/letter.rs
+++ src/letter.rs
@@ -3,25 +3,35 @@ use std::fmt;
 #[derive(PartialEq, PartialOrd, Ord, Eq, Debug, Clone, Copy, Default)]
 pub struct Letter(u8);
 
-const ALPHABET: [char; 27] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', '\'',
-];
-
 impl Letter {
+    pub const ALPHABET: [char; 27] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\'',
+    ];
+
+    pub const ALPHABET_SIZE: usize = Letter::ALPHABET.len();
+
     pub fn to_char(&self) -> char {
-        ALPHABET[self.0 as usize]
+        Letter::ALPHABET[self.0 as usize]
     }
 
     pub fn to_u32(&self) -> u32 {
         self.0 as u32
+    }
+
+    pub fn to_usize(&self) -> usize {
+        self.0 as usize
+    }
+
+    pub fn to_u8(&self) -> u8 {
+        self.0
     }
 }
 
 impl fmt::Display for Letter {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let Letter(inx) = self;
-        write!(f, "{}", ALPHABET[*inx as usize])
+        write!(f, "{}", Letter::ALPHABET[*inx as usize])
     }
 }
 
@@ -45,17 +55,11 @@ impl TryFrom<u32> for Letter {
     type Error = &'static str;
 
     fn try_from(value: u32) -> Result<Self, Self::Error> {
-        if value > 31 {
+        if value >= Letter::ALPHABET.len() as u32 {
             Err("Letter only accepts u32 values in the range 0..=31.")
         } else {
             Ok(Letter(value as u8))
         }
-    }
-}
-
-impl Into<char> for Letter {
-    fn into(self) -> char {
-        'a'
     }
 }
 
@@ -66,7 +70,7 @@ mod tests {
 
     #[test]
     fn display_is_the_character_from_alphabet_indexed() {
-        for c in ALPHABET {
+        for c in Letter::ALPHABET {
             let actual = c.to_string();
             let expected = c.to_string();
             assert_eq!(actual, expected);
@@ -74,8 +78,8 @@ mod tests {
     }
 
     #[test]
-    fn try_from_when_in_alphabet() {
-        for c in ALPHABET {
+    fn try_from_char_when_in_alphabet() {
+        for c in Letter::ALPHABET {
             let letter = Letter::try_from(c);
             match letter {
                 Err(_) => panic!("Could not convert the character '{}' to a Letter.", c),
@@ -85,7 +89,7 @@ mod tests {
     }
 
     #[test]
-    fn try_from_when_not_in_alphabet() {
+    fn try_from_char_when_not_in_alphabet() {
         let before_a = char::from_u32(('a' as u32) - 1).unwrap();
         let after_z = char::from_u32(('z' as u32) + 1).unwrap();
         let invalid_character = ['4', '$', 'A', 'B', 'Z', before_a, after_z];
@@ -99,18 +103,41 @@ mod tests {
     }
 
     #[test]
+    fn try_from_u32_when_not_in_alphabet() {
+        assert!(Letter::try_from(Letter::ALPHABET.len() as u32).is_err());
+        assert!(Letter::try_from((Letter::ALPHABET.len() + 1) as u32).is_err());
+    }
+
+    #[test]
+    fn try_from_u32_when_in_alphabet() {
+        for i in 0..Letter::ALPHABET.len() - 1 {
+            assert!(Letter::try_from(i as u32).is_ok());
+        }
+    }
+
+    #[test]
     fn to_char() {
-        for expected in ALPHABET {
+        for expected in Letter::ALPHABET {
             let actual: char = Letter::try_from(expected).unwrap().to_char();
             assert_eq!(expected, actual);
         }
     }
 
     #[test]
+    fn to_u8() {
+        for expected in Letter::ALPHABET {
+            let letter = Letter::try_from(expected).unwrap();
+            let actual = letter.to_u8();
+            let expected = letter.0;
+            let expected = assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
     fn to_u32_returns_index_into_alphabet() {
-        for c in ALPHABET {
+        for c in Letter::ALPHABET {
             let actual = Letter::try_from(c).unwrap().to_u32();
-            let expected = ALPHABET
+            let expected = Letter::ALPHABET
                 .iter()
                 .enumerate()
                 .find_map(|(inx, char)| match *char == c {
@@ -121,5 +148,27 @@ mod tests {
                 .unwrap();
             assert_eq!(expected, actual);
         }
+    }
+
+    #[test]
+    fn to_usize_returns_index_into_alphabet() {
+        for c in Letter::ALPHABET {
+            let actual = Letter::try_from(c).unwrap().to_usize();
+            let expected = Letter::ALPHABET
+                .iter()
+                .enumerate()
+                .find_map(|(inx, char)| match *char == c {
+                    true => Some(inx),
+                    false => None,
+                })
+                .map(|inx| inx as usize)
+                .unwrap();
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn alphabet_size_constant() {
+        assert_eq!(Letter::ALPHABET_SIZE, Letter::ALPHABET.len());
     }
 }
