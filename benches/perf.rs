5df1d638d242b1b7225c129a3500d49a7a95b232
jmagaram <justin@magaram.com>
2024-04-16 15:52:16 -0700

    add subset speed score

--- benches/perf.rs
+++ benches/perf.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use keylayout::{dictionary::Dictionary, key::Key, keyboard::Keyboard, penalty::Penalty};
 
 fn calculate_penalty_score(c: &mut Criterion) {
@@ -7,7 +7,7 @@ fn calculate_penalty_score(c: &mut Criterion) {
     c.bench_function("CALCULATE PENALTY", |b| {
         b.iter(|| {
             let keys = d.alphabet().random_subsets(&layout).collect::<Vec<Key>>();
-            let _keyboard = Keyboard::new_from_keys(keys).penalty(&d, Penalty::MAX);
+            let _keyboard = Keyboard::new_from_keys(keys).penalty(&d, black_box(Penalty::MAX));
             ()
         })
     });
@@ -23,7 +23,7 @@ fn spell_every_word(c: &mut Criterion) {
             d.words()
                 .iter()
                 .map(|w| {
-                    keyboard.spell(w);
+                    keyboard.spell(black_box(w));
                 })
                 .count();
             ()
@@ -34,15 +34,39 @@ fn spell_every_word(c: &mut Criterion) {
 fn load_dictionary(c: &mut Criterion) {
     c.bench_function("LOAD DICTIONARY", |b| {
         b.iter(|| {
-            let _d = Dictionary::load().with_top_n_words(1000);
+            let _d = Dictionary::load().with_top_n_words(black_box(1000));
         })
     });
 }
 
+fn generate_big_subsets(c: &mut Criterion) {
+    c.bench_function("GENERATE BIG SUBSETS", |b| {
+        b.iter(|| {
+            let key = Key::with_every_letter();
+            let sizes = [7];
+            for s in sizes {
+                key.subsets_of_size(black_box(s)).count();
+            }
+        })
+    });
+}
+
+fn generate_small_subsets(c: &mut Criterion) {
+    c.bench_function("GENERATE SMALL SUBSETS", |b| {
+        b.iter(|| {
+            let key = Key::with_every_letter();
+            let sizes = [1, 2, 3, 4];
+            for s in sizes {
+                key.subsets_of_size(black_box(s)).count();
+            }
+        })
+    });
+}
 criterion_group!(
     benches,
-    load_dictionary,
-    calculate_penalty_score,
-    spell_every_word
+    generate_big_subsets,
+    generate_small_subsets // load_dictionary,
+                           // calculate_penalty_score,
+                           // spell_every_word
 );
 criterion_main!(benches);
