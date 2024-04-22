ebb1dc233ef530e8f0bb5e6db2586f84c97ba6cd
jmagaram <justin@magaram.com>
2024-04-22 10:52:47 -0700

    simplify main

--- src/main.rs
+++ src/main.rs
@@ -19,39 +19,32 @@ mod tally;
 mod util;
 mod word;
 
-enum Run {
-    BestNKey(u32),
-    Other,
-}
-
 fn main() {
-    // For bad pairs, they show up around 90
-    // For bad triples, they show up around 1880
-
     let dict = Dictionary::load();
 
-    let other = Run::Other;
-
-    let best_n_key = Run::BestNKey(18);
-
-    let run = other;
-
-    match run {
-        Run::BestNKey(count) => {
-            exhaustive::best_n_key(count);
-        }
-        Run::Other => {
-            exhaustive::run_dfs();
-            // for r in evolution::find_best(&dict, 23, Penalty::new(0.001)) {
-            //     match r {
-            //         None => println!("Nothing found"),
-            //         Some(r) => {
-            //             println!("{}", r);
-            //         }
-            //     }
-            // }
-            // exhaustive::best_n_key(23);
-            // exhaustive::dumb_run_dfs();
+    fn best_n_key(key_count: u32) {
+        match exhaustive::best_n_key(key_count) {
+            None => {
+                println!("No solution found")
+            }
+            Some(best) => {
+                println!("Done searching for best {} key keyboard.", key_count);
+                println!("{}", best);
+            }
         }
     }
+
+    fn find_best_dfs() {
+        exhaustive::run_dfs();
+    }
+
+    let run_genetic_solver = move || {
+        for result in evolution::find_best(&dict, 10, Penalty::new(0.0001)) {
+            if let Some(solution) = result {
+                println!("{}", solution);
+            }
+        }
+    };
+
+    run_genetic_solver();
 }
