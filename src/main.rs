0f7a467f3b61679edc6a8b46e446fb5b698f6e94
jmagaram <justin@magaram.com>
2024-04-29 18:10:39 -0700

    reorganize main

--- src/main.rs
+++ src/main.rs
@@ -30,40 +30,41 @@ fn generate_keyboard_stats() {
     generate_stats::random_keyboards(samples, &dictionary, &file_name).unwrap();
 }
 
+fn dfs_pruning() {
+    dfs_pruning::solve();
+}
+
+fn find_best_n_key() {
+    let dict = Dictionary::load();
+    let best = exhaustive_n_key::find_best_n_key(25, &dict);
+    match best {
+        None => {
+            println!("None found");
+        }
+        Some(k) => {
+            println!("{}", k);
+        }
+    }
+}
+
+fn genetic_solver() {
+    let dict = Dictionary::load();
+    let prohibited = Prohibited::with_top_n_letter_pairs(&dict, 50);
+    let args = genetic::FindBestArgs {
+        dictionary: &dict,
+        die_threshold: Penalty::new(0.00001),
+        key_count: 10,
+        prohibited,
+    };
+    for result in genetic::find_best(args) {
+        if let Some(solution) = result {
+            println!("{}", solution);
+        }
+    }
+}
+
 fn main() {
-    let dfs_pruning = || {
-        dfs_pruning::solve();
-    };
-
-    let find_best_n_key = || {
-        let dict = Dictionary::load();
-        let best = exhaustive_n_key::find_best_n_key(25, &dict);
-        match best {
-            None => {
-                println!("None found");
-            }
-            Some(k) => {
-                println!("{}", k);
-            }
-        }
-    };
-
-    let genetic_solver = move || {
-        let dict = Dictionary::load();
-        let prohibited = Prohibited::with_top_n_letter_pairs(&dict, 50);
-        let args = genetic::FindBestArgs {
-            dictionary: &dict,
-            die_threshold: Penalty::new(0.00001),
-            key_count: 10,
-            prohibited,
-        };
-        for result in genetic::find_best(args) {
-            if let Some(solution) = result {
-                println!("{}", solution);
-            }
-        }
-    };
-
     // dfs_pruning();
-    generate_keyboard_stats();
+    // generate_keyboard_stats();
+    dfs_pruning();
 }
