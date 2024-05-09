e3d10729e45ac5231259b71bb75ec8c14b7728e4
jmagaram <justin@magaram.com>
2024-05-08 23:39:21 -0700

    clean up pairing

--- src/pairing.rs
+++ src/pairing.rs
@@ -1,13 +1,16 @@
 use crate::{
     dictionary::Dictionary, key::Key, keyboard::Keyboard, penalty::Penalty,
-    single_key_penalties::SingleKeyPenalties, tally::Tally,
+    single_key_penalties::SingleKeyPenalties, solution::Solution, tally::Tally,
 };
 use crossbeam_channel::*;
 use humantime::{format_duration, FormattedDuration};
 use std::{
     cell::{Cell, RefCell},
     ops::Deref,
-    sync::{Arc, Mutex},
+    sync::{
+        atomic::{AtomicBool, AtomicU64, Ordering},
+        Arc, Mutex,
+    },
     thread::{spawn, JoinHandle},
     time::{Duration, Instant},
 };
@@ -124,6 +127,120 @@ where
 }
 
 impl Args {
+    fn spawn_keyboard_generator(
+        sender: Sender<Keyboard>,
+        alphabet: Key,
+        generated_all_keyboards: Arc<AtomicBool>,
+        max_key_size: u8,
+        pairs_to_ignore: usize,
+        ten_key_threshold: Penalty,
+    ) -> JoinHandle<bool> {
+        let penalties = SingleKeyPenalties::load();
+        let pairs = {
+            let mut pairs = penalties.of_key_size(2).collect::<Vec<(Key, Penalty)>>();
+            pairs.sort_by(|a, b| a.1.cmp(&b.1));
+            pairs
+                .iter()
+                .take(pairs.len() - pairs_to_ignore)
+                .map(|(k, _p)| *k)
+                .collect::<Vec<Key>>()
+        };
+        let pruned: Cell<u64> = Cell::new(0);
+        let pruned_at: RefCell<Tally<usize>> = RefCell::new(Tally::new());
+        let prune = move |k: &Keyboard| {
+            let key_count = k.len();
+            let prune_from = 10;
+            let prune_to = 18;
+            if key_count >= prune_from && key_count <= prune_to {
+                let factor = 0.85f32.powi(key_count as i32 - 10);
+                let threshold_for_key_count = ten_key_threshold.to_f32() * factor;
+                let estimate = k.penalty_estimate(&penalties);
+                let should_prune = estimate.to_f32() > threshold_for_key_count;
+                if should_prune {
+                    pruned_at.borrow_mut().increment(key_count);
+                    pruned.set(pruned.get() + 1);
+                    if pruned.get().rem_euclid(10_000_000) == 0 {
+                        println!("Pruned {}", pruned.get().separate_with_underscores());
+                        (prune_from..=prune_to).for_each(|key_count| {
+                            let total = pruned_at.borrow().count(&key_count);
+                            if total > 0 {
+                                println!(
+                                    "Pruned size {:<2} : {}",
+                                    key_count,
+                                    total.separate_with_underscores()
+                                );
+                            }
+                        });
+                    }
+                }
+                should_prune
+            } else {
+                false
+            }
+        };
+        let created = Cell::new(0);
+        let created_tally = RefCell::new(Tally::new());
+        spawn(move || {
+            let args = BuildKeyboardsArgs {
+                pairs: &pairs,
+                pairs_index: 0,
+                k: Keyboard::with_every_letter_on_own_key(alphabet),
+                prohibited: vec![],
+                channel: &sender,
+                max_key_size,
+                created: &created,
+                created_tally: &created_tally,
+                prune: &prune,
+                report_every_created: 10_000_000,
+            };
+            args.build_keyboards();
+            generated_all_keyboards.fetch_or(true, Ordering::Relaxed)
+        })
+    }
+
+    fn spawn_keyboard_evaluator(
+        receiver: Receiver<Keyboard>,
+        dictionary: Arc<Dictionary>,
+        best: Arc<Mutex<Solution>>,
+        generated_all_keyboards: Arc<AtomicBool>,
+        keyboards_seen: Arc<AtomicU64>,
+        start_time: Instant,
+    ) -> JoinHandle<()> {
+        spawn(move || loop {
+            match receiver.recv_timeout(Duration::from_secs(60)) {
+                Ok(kbd) => {
+                    let best_penalty = best.lock().map(|s| s.penalty()).unwrap_or(Penalty::MAX);
+                    let penalty = kbd.penalty(&dictionary, best_penalty);
+                    let count = keyboards_seen.fetch_add(1, Ordering::Relaxed);
+                    if penalty < best_penalty {
+                        if let Ok(solution) = best.lock().as_deref_mut() {
+                            let new_solution = kbd.to_solution(
+                                penalty,
+                                format!("kbd {}", count.separate_with_underscores()),
+                            );
+                            println!("{}", new_solution);
+                            *solution = new_solution;
+                        }
+                    } else if count.rem_euclid(250_000) == 0 {
+                        println!(
+                            "Evaluated: {} in {}",
+                            count.separate_with_underscores(),
+                            start_time.elapsed().round_to_seconds()
+                        );
+                        if let Ok(solution) = best.lock().as_deref() {
+                            println!("{}", solution);
+                        }
+                    }
+                }
+                Err(_) => {
+                    if generated_all_keyboards.load(Ordering::Relaxed) {
+                        break;
+                    }
+                }
+            }
+        })
+    }
+
     // Starts with a keyboard with every letter having its own key. Then try to combine
     // letters recursively until there are 10 keys. Evaulate the score of every 10 key
     // keyboard and compare it to the best so far. Combining occurs by making a list of
@@ -133,129 +250,35 @@ impl Args {
     pub fn solve(&self) {
         use crossbeam_channel::*;
         use std::sync::atomic::*;
-        let d = Arc::new(Dictionary::load());
+        let dictionary = Arc::new(Dictionary::load());
         let best = Arc::new(Mutex::new(
             Keyboard::empty().to_solution(Penalty::MAX, "".to_string()),
         ));
         let start_time = Instant::now();
-        let done_generating = Arc::new(AtomicBool::new(false));
-        let (sdr, rcr) = bounded::<Keyboard>(1_000_000);
-        let spawn_keyboard_generator = || {
-            let sdr = sdr.clone();
-            let d = d.clone();
-            let done_generating_keyboards = done_generating.clone();
-            let single_key_penalties = SingleKeyPenalties::load();
-            let pairs_to_consider = {
-                let mut pairs = single_key_penalties
-                    .of_key_size(2)
-                    .collect::<Vec<(Key, Penalty)>>();
-                pairs.sort_by(|a, b| a.1.cmp(&b.1));
-                pairs
-                    .iter()
-                    .take(pairs.len() - self.pairings_to_ignore as usize)
-                    .map(|(k, _p)| *k)
-                    .collect::<Vec<Key>>()
-            };
-            let max_key_size = self.max_key_size;
-            let pruned: Cell<u64> = Cell::new(0);
-            let pruned_at: RefCell<Tally<usize>> = RefCell::new(Tally::new());
-            let prune_threshold = self.prune_threshold;
-            let prune = move |k: &Keyboard| {
-                let key_count = k.len();
-                let prune_from = 10;
-                let prune_to = 18;
-                if key_count >= prune_from && key_count <= prune_to {
-                    let factor = 0.85f32.powi(key_count as i32 - 10);
-                    let threshold_by_key_count = prune_threshold.to_f32() * factor;
-                    let estimate = k.penalty_estimate(&single_key_penalties);
-                    let should_prune = estimate.to_f32() > threshold_by_key_count;
-                    if should_prune {
-                        pruned_at.borrow_mut().increment(key_count);
-                        pruned.set(pruned.get() + 1);
-                        if pruned.get().rem_euclid(10_000_000) == 0 {
-                            println!("Pruned {}", pruned.get().separate_with_underscores());
-                            (prune_from..=prune_to).for_each(|key_count| {
-                                let total = pruned_at.borrow().count(&key_count);
-                                if total > 0 {
-                                    println!(
-                                        "Pruned size {:<2} : {}",
-                                        key_count,
-                                        total.separate_with_underscores()
-                                    );
-                                }
-                            });
-                        }
-                    }
-                    should_prune
-                } else {
-                    false
-                }
-            };
-            spawn(move || {
-                let created = Cell::new(0);
-                let created_tally = RefCell::new(Tally::new());
-                let args = BuildKeyboardsArgs {
-                    pairs: &pairs_to_consider,
-                    pairs_index: 0,
-                    k: Keyboard::with_every_letter_on_own_key(d.alphabet()),
-                    prohibited: vec![],
-                    channel: &sdr,
-                    max_key_size,
-                    created: &created,
-                    created_tally: &created_tally,
-                    prune: &prune,
-                    report_every_created: 10_000_000,
-                };
-                args.build_keyboards();
-                done_generating_keyboards.fetch_or(true, Ordering::Relaxed)
-            })
-        };
-        let kbd_seen = Arc::new(AtomicU64::new(0));
-        let spawn_keyboard_evaluator = || {
-            let rcr = rcr.clone();
-            let d = d.clone();
-            let best = best.clone();
-            let done_generating_keyboards = done_generating.clone();
-            let kbd_seen = kbd_seen.clone();
-            spawn(move || loop {
-                match rcr.recv_timeout(Duration::from_secs(60)) {
-                    Ok(kbd) => {
-                        let best_penalty = best.lock().map(|s| s.penalty()).unwrap_or(Penalty::MAX);
-                        let penalty = kbd.penalty(&d, best_penalty);
-                        let seen = kbd_seen.fetch_add(1, Ordering::Relaxed);
-                        if penalty < best_penalty {
-                            if let Ok(solution) = best.lock().as_deref_mut() {
-                                let new_solution = kbd.to_solution(
-                                    penalty,
-                                    format!("kbd {}", seen.separate_with_underscores()),
-                                );
-                                println!("{}", new_solution);
-                                *solution = new_solution;
-                            }
-                        } else if seen.rem_euclid(250_000) == 0 {
-                            println!(
-                                "Evaluated: {} in {}",
-                                seen.separate_with_underscores(),
-                                start_time.elapsed().round_to_seconds()
-                            );
-                            if let Ok(solution) = best.lock().as_deref() {
-                                println!("{}", solution);
-                            }
-                        }
-                    }
-                    Err(_) => {
-                        if done_generating_keyboards.load(Ordering::Relaxed) {
-                            break;
-                        }
-                    }
-                }
-            })
-        };
-        let generate_keyboards = spawn_keyboard_generator(); // make only 1!
+        let generated_all_keyboards = Arc::new(AtomicBool::new(false));
+        let (sender, receiver) = bounded::<Keyboard>(100_000);
+        let keyboards_seen = Arc::new(AtomicU64::new(0));
+        let generator = Self::spawn_keyboard_generator(
+            sender,
+            dictionary.alphabet(),
+            generated_all_keyboards.clone(),
+            self.max_key_size,
+            70,
+            self.prune_threshold,
+        );
         let evaluators = (1..=self.threads)
-            .map(|_| spawn_keyboard_evaluator())
+            .map(|_| {
+                Self::spawn_keyboard_evaluator(
+                    receiver.clone(),
+                    dictionary.clone(),
+                    best.clone(),
+                    generated_all_keyboards.clone(),
+                    keyboards_seen.clone(),
+                    start_time,
+                )
+            })
             .collect::<Vec<JoinHandle<_>>>();
-        generate_keyboards.join().unwrap();
+        generator.join().unwrap();
         for i in evaluators.into_iter() {
             i.join().unwrap();
         }
