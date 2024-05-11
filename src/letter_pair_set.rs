a2ceb0ccaf01f574e6cdd87bfaad3b9f001a2e40
jmagaram <justin@magaram.com>
2024-05-10 18:19:25 -0700

    tests

--- src/letter_pair_set.rs
+++ src/letter_pair_set.rs
@@ -1,8 +1,6 @@
-use std::fmt;
-
-use hashbrown::HashSet;
-
 use crate::letter_pair::LetterPair;
+use hashbrown::HashSet;
+use std::fmt;
 
 #[derive(Hash)]
 pub struct LetterPairSet(Vec<LetterPair>);
@@ -29,5 +27,47 @@ impl fmt::Display for LetterPairSet {
             .collect::<Vec<String>>()
             .join(",");
         write!(f, "{}", parts)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::letter::Letter;
+
+    use super::*;
+
+    #[test]
+    fn display_is_empty_string_if_no_pairs() {
+        let actual = LetterPairSet::new(vec![].into_iter());
+        assert_eq!("", actual.to_string());
+    }
+
+    #[test]
+    fn duplicates_are_removed() {
+        let pair = LetterPair::new(Letter::new('a'), Letter::new('b'));
+        let set = LetterPairSet::new(vec![pair.clone(), pair.clone(), pair.clone()].into_iter());
+        assert_eq!("ab", set.to_string());
+    }
+
+    #[test]
+    fn display_test() {
+        let data = [
+            ("ab,cd,ef", "ab,cd,ef"),
+            ("ef,cd,ab", "ab,cd,ef"),
+            ("fe,dc,ba", "ab,cd,ef"),
+            ("ab", "ab"),
+            ("ba", "ab"),
+        ];
+        for (input, expected) in data {
+            let pairs = input.split(',').map(|pr| {
+                let a = Letter::new(pr.chars().into_iter().nth(0).unwrap());
+                let b = Letter::new(pr.chars().into_iter().nth(1).unwrap());
+                LetterPair::new(a, b)
+            });
+            let set = LetterPairSet::new(pairs);
+            let actual = set.to_string();
+            assert_eq!(actual, expected);
+        }
     }
 }
